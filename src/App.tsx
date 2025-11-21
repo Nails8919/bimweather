6fce2b58de6007ca62c4d3cf59e2ef6041e6c7b5
Nails8919 <shorty_love_12@hotmail.com>
2025-11-21 15:15:42 -0400

    UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,7 @@ function App() {
     wind: {
       speed: number,
       deg: number,
-      gust: number
-      icon: string
+      gust?: number // Made gust optional as it may not always be present
     }
   }
 
@@ -47,6 +46,7 @@ function App() {
       })
     //.catch(err => console.error(err))
   }
+
   // Fetch weather data on component mount
   useEffect(() => {
     getweatherData({ lat: 13.1667, lon: -59.5333 }) // Coordinates for Bridgetown, Barbados
@@ -61,24 +61,26 @@ function App() {
             Welcome to Bim Weather App
           </p>
           <div className="flex flex-col items-center mt-12">
-            <div className=" bg-white h-full w-full bg-opacity-70 rounded-lg text-center shadow-lg p-6 m-6 w-96">
-              <p className="text-lg text-gray-700 font-semibold mt-4">Your personalized weather dashboard</p>
-              <div className="flex flex items-center gap-4 mt-6">
-                <div className="mt-4 font-semibold"> Temperature: <p>{weatherData.main.temp}&deg;C</p></div>
-                <div className="mt-2 font-semibold"> Feels Like: 
+            <div className="border-2 border-blue-500 rounded-lg p-6 bg-white h-full w-full bg-opacity-70 rounded-lg text-center shadow-lg p-6 m-6 w-96">
+              <p className="text-3xl text-gray-700 font-semibold underline mt-4">Your Personalized Weather Dashboard</p>
+              <div className="flex flex items-baseline justify-center gap-4 mt-6 ">
+                <div className="font-semibold"> Temperature: <p>{weatherData.main.temp}&deg;C</p></div>
+                <div className="font-semibold"> Feels Like:
                   <p>{weatherData.main.feels_like}&deg;C</p></div>
-                <div className="mt-2 font-semibold"> Humidity: <p>{weatherData.main.humidity}%</p>  </div>
-                <div className="mt-2 font-semibold"> Pressure: <p>{weatherData.main.pressure} hPa</p></div>
-                <div className="mt-2 font-semibold"> Min Temperature: <p>{weatherData.main.temp_min}&deg;C</p></div>
-                <div className="mt-2 font-semibold"> Max Temperature: <p>{weatherData.main.temp_max}&deg;C</p></div>
+                <div className="font-semibold"> Humidity: <p>{weatherData.main.humidity}%</p>  </div>
+                <div className="font-semibold"> Pressure: <p>{weatherData.main.pressure} hPa</p></div>
+                <div className="font-semibold"> Min Temperature: <p>{weatherData.main.temp_min}&deg;C</p></div>
+                <div className="font-semibold"> Max Temperature: <p>{weatherData.main.temp_max}&deg;C</p></div>
               </div>
-              <div className="flex flex items-center gap-4 mt-6">
-                <div className="mt-2 font-semibold"> Sea Level: <p>{weatherData.main.sea_level} hPa</p></div>
-                <div className="mt-2 font-semibold mb-6"> Ground Level: <p>{weatherData.main.grnd_level} hPa</p></div>
-                <div className="mt-2 font-semibold mb-6"> Current Weather Description: <p>{weatherData.weather[0].description} {<img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather icon" />}</p></div>
-                <div className="mt-2 font-semibold mb-6"> Wind Speed: <p>{weatherData.wind.speed} m/s</p></div>
-                <div className="mt-2 font-semibold mb-6"> Wind Direction: <p>{cardinalFromDegree(weatherData.wind.deg, CardinalSubset.Intercardinal)}</p></div>
-                <div className="mt-2 font-semibold mb-6"> Wind Gust: <p>{weatherData.wind.gust} m/s</p></div>
+              <div className="flex flex items-baseline justify-center gap-4 mt-6 ">
+                <div className="font-semibold"> Sea Level: <p>{weatherData.main.sea_level} hPa</p></div>
+                <div className="font-semibold"> Ground Level: <p>{weatherData.main.grnd_level} hPa</p></div>
+                <div className="font-semibold text-center"> Current Weather Description: <p>{weatherData.weather[0].description} {<img src={`https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather icon" className="block flex justify-center mx-auto" />}</p></div>
+                <div className="font-semibold"> Wind Speed: <p>{weatherData.wind.speed} m/s
+                  {weatherData.wind.gust != null && ` with gusts up to ${weatherData.wind.gust} m/s`} in a {cardinalFromDegree(weatherData.wind.deg, CardinalSubset.Intercardinal)} direction
+                </p></div>
+                {/* <div className="font-semibold"> Wind Direction: <p>{cardinalFromDegree(weatherData.wind.deg, CardinalSubset.Intercardinal)} </p></div> */}
+                {/* <div className="font-semibold"> Wind Gust: <p>{cardinalFromDegree(weatherData.wind.gust, CardinalSubset.Intercardinal)} m/s</p></div> */}
               </div>
             </div>
           </div>
